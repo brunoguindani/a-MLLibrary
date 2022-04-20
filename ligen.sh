803e63babe402d3fb3e95e092e2f78839508aa06
brunoguindani <bruno@guindani.org>
2022-04-20 17:21:31 +0200

    Updated subfolder of Ligen ini files

--- ligen.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-python3 ./run.py -c example_configurations/ligen01_time.ini -o outputs/ligen01/time_SFS
-python3 ./run.py -c example_configurations/ligen01_through.ini -o outputs/ligen01/through_SFS
