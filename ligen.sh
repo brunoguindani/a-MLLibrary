5a2e10f32efc008e31434622a3871325bf1cd9ff
brunoguindani <bruno@guindani.org>
2022-04-20 10:56:10 +0200

    Updated ligen.sh

--- ligen.sh
+++ ligen.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-python3 ./run.py -c example_configurations/ligen01_time.ini -o outputs/ligen01/time
-python3 ./run.py -c example_configurations/ligen01_through.ini -o outputs/ligen01/through
+python3 ./run.py -c example_configurations/ligen01_time.ini -o outputs/ligen01/time_SFS
+python3 ./run.py -c example_configurations/ligen01_through.ini -o outputs/ligen01/through_SFS
